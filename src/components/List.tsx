5bb64726203511fafc3323c332bc920734fa8f25
jholloway <jholloway@griddynamics.com>
2023-01-26 16:18:17 -0500

    add counter feature to Main.tsx

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { VoidExpression } from 'typescript';
 
 interface Props{
     item: string,
+    quantity: number,
+    increase: (params: string) => void,
+    decrease: (params: string) => void,
     delete: (params : string) => void
 }
 
@@ -9,7 +13,12 @@ const List = (props: Props) => {
     return (
         <div className='list-item'>
             <p className='item-name'>{props.item}</p>
-            <button id='delete-btn' title='Delete' onClick={() => props.delete(props.item)}><i className="fa fa-trash-o"></i></button>
+            <div className='buttons'>
+                <button className='arrow-btns' title='Subtract' onClick={() => props.decrease(props.item)}><i className="fa fa-caret-left"></i></button>
+                <p className='quantity'>{props.quantity}</p>
+                <button className='arrow-btns' title='Add' onClick={() => props.increase(props.item)}><i className="fa fa-caret-right"></i></button>
+                <button id='delete-btn' title='Delete' onClick={() => props.delete(props.item)}><i className="fa fa-trash-o"></i></button>
+            </div> 
         </div>
     );
 };
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,41 +1,88 @@
 import React, {FC, useState} from 'react';
 import List from './List';
 
+interface Item{
+    name: string,
+    quantity: number
+}
+
 const Main : FC = () => {
-    const [item, setItem] = useState<string>("")
-    const [allItems, setAllItems] = useState<string[]>([])
+    const [item, setItem] = useState<Item>({
+        name: '',
+        quantity: 0
+    })
+    const [allItems, setAllItems] = useState<Item[]>([])
     const items = allItems.map(i => {
-        return <List key={i} item= {i} delete={deleteItem}/>
+        return <List 
+                key={i.name} 
+                item= {i.name} 
+                increase={increaseQuantity}
+                decrease={decreaseQuantity}
+                quantity={i.quantity} 
+                delete={deleteItem}
+                />
     })
 
     function handleChange(event: React.SyntheticEvent){
         const target = event.target as HTMLInputElement
         const value = target.value
-        setItem(value)
+        const name = target.name
+        setItem(prevItem => ({
+            ...prevItem,
+            [name]: value
+        }))
     }
 
     function addItem(event : React.SyntheticEvent){
         event.preventDefault()
-        if(item === ''){
+        if(item.name === ''){
             alert('Please enter an item')
+        }else if(item.quantity === 0 || (item.quantity).toString().match(/[a-zA-Z]/g)){
+            alert('Please enter an appropriate quantity')
         }else{
             setAllItems(prevArr => [...prevArr, item])
-            setItem('')
+            setItem({
+                name: '',
+                quantity: 0
+            })
         }   
     }
 
+    function increaseQuantity(itemName: string){
+        const newItemsArr = allItems.map(item => {
+            if(item.name === itemName){
+                const newQuantity = Number(item.quantity) + 1
+                return{...item, quantity: newQuantity}
+            }else{
+                return item
+            }
+        })
+        setAllItems(newItemsArr)
+    }
+
+    function decreaseQuantity(itemName: string){
+        const newItemsArr = allItems.map(item => {
+            if(item.name === itemName){
+                const newQuantity = Number(item.quantity) - 1
+                return{...item, quantity: newQuantity}
+            }else{
+                return item
+            }
+        })
+        setAllItems(newItemsArr)
+    }
+
     function deleteItem(itemName: string){
-        const newArray = allItems.filter(word => word !== itemName)
+        const newArray = allItems.filter(word => word.name !== itemName)
         setAllItems(newArray)
     }
 
 
-    console.log(item)
-    console.log(allItems)
     return (
         <main>
             <form className='input-area'>
-                <input id='input-field' type="text" placeholder='Enter item here (one item at a time)' name='itemName' value={item} onChange={handleChange}></input>
+                <input id='input-field' type="text" placeholder='Enter a item' name='name' value={item.name} onChange={handleChange}></input>
+                <input title='Enter a quantity' id='quantity-field' type="text" placeholder='Enter the quantity' name="quantity" value={item.quantity} onChange={handleChange}></input>
                 <button id='submit-btn' type='submit' onClick={addItem}>Enter</button>
             </form>
             <div className='display-area'>
