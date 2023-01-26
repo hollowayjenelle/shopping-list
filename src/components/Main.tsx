68c5ecb5fddffac89936d47d598a69e782a0f21e
jholloway <jholloway@griddynamics.com>
2023-01-26 10:46:09 -0500

    rename InputField to Main

---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+import {FC, useState} from 'react';
+
+const Main : FC = () => {
+    const [item, setItem] = useState<string>("")
+    const [allItems, setAllItems] = useState<string[]>([])
+
+    return (
+        <main>
+            
+        </main>
+    );
+};
+
+export default Main;
